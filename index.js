b9b96b1183114261d7a4b8fba3f1518c296f9f50
Paul Holden <pholden@stria.com>
2019-08-29 13:36:50 -0700

    Switch to using rc files

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const program = require('commander');
 require('tedious'); // Require for pkg to pick up dependency
 const Sequelize = require('sequelize');
 const SharePoint = require('@paulholden/sharepoint');
-const dotenv = require('dotenv');
 const loadOrmModels = require('./lib/orm.js');
 const Application = require('./lib/application.js');
+const rc = require('rc');
 
-// Load .env configuration
-dotenv.config();
+const APP = 'aerasp';
+const conf = rc(APP);
+
+if (!conf.configs) {
+  console.error('No config files were found. Configuration is loaded from:');
+  console.error('');
+  console.error(' 1) Any file passed via --config argument');
+  console.error(` 2) Any .${APP}rc file found in local or parent directories`)
+  console.error(` 3) $HOME/.${APP}rc`);
+  console.error(` 4) $HOME/.${APP}/config`);
+  console.error(` 5) $HOME/.config/${APP}`);
+  console.error(` 6) $HOME/.config/${APP}/config`);
+  console.error(` 7) /etc/${APP}rc`);
+  console.error(` 8) /etc/${APP}/config`);
+  console.error('');
+  console.error('Configurations are loaded in JSON or INI format.');
+  console.error('Data is merged down; earlier configs override those that follow.');
+  return;
+}
 
 // Process commandline arguments
 program.option('-d, --debug', 'extra debugging output');
@@ -16,17 +33,18 @@ program.option('-R, --retry-only', 'only retry failed migrations');
 program.parse(process.argv);
 
 // Sequelize connection
-let sequelize = new Sequelize('Aera Energy', process.env.MSSQL_USERNAME, process.env.MSSQL_PASSWORD, {
+let sequelize = new Sequelize('Aera Energy', conf.mssql.username, conf.mssql.password, {
   host: 'STRIA-SQL1',
   dialect: 'mssql',
   logging: !!program.debug ? console.log : false
 });
 // SharePoint site
-let sharepoint = new SharePoint(process.env.SHAREPOINT_URL);
+let sharepoint = new SharePoint(conf.sharepoint.url);
 let options = {
   // Where the program will check for files to migrate
-  outputDirectory: process.env.DELIVERY_OUTPUT_DIR,
-  migrationTable: 'TestWellLogMigrations' // TODO
+  outputDirectory: conf.deliveryOutputDir,
+  // The SQL table that will contain migrated file details
+  migrationTable: conf.migrationTable
 };
 // Load ORM models
 let models = loadOrmModels(sequelize, options);
@@ -35,7 +53,7 @@ sequelize.authenticate().then(res => {
   // Leave force: false unless you want to summon hellfire
   return sequelize.sync({ force: false });
 }).then(() => {
-  return sharepoint.authenticate(process.env.SHAREPOINT_USERNAME, process.env.SHAREPOINT_PASSWORD);
+  return sharepoint.authenticate(conf.sharepoint.username, conf.sharepoint.password);
 }).then(() => {
   return sharepoint.getWebEndpoint()
 }).then(async () => {
