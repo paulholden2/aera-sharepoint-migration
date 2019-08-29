3c70e8521f5f70f39dbd1eab67b81e01062bfb9d
Paul Holden <pholden@stria.com>
2019-08-29 11:25:13 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const program = require('commander');
+const Sequelize = require('sequelize');
+const SharePoint = require('@paulholden/sharepoint');
+const dotenv = require('dotenv');
+const orm = require('./lib/orm.js');
+const Application = require('./lib/application.js');
+
+// Load .env configuration
+dotenv.config();
+
+// Process commandline arguments
+program.option('-d, --debug', 'extra debugging output');
+program.option('-O, --orm-only', 'only sync ORM with database')
+program.option('-R, --retry-only', 'only retry failed migrations');
+program.parse(process.argv);
+
+// Sequelize connection
+let sequelize = new Sequelize('Aera Energy', process.env.MSSQL_USERNAME, process.env.MSSQL_PASSWORD, {
+  host: 'STRIA-SQL1',
+  dialect: 'mssql',
+  logging: !!program.debug ? console.log : false
+});
+
+// SharePoint site
+let sharepoint = new SharePoint(process.env.SHAREPOINT_URL);
+
+// Load ORM models
+let models = orm(sequelize);
+
+sequelize.authenticate().then(res => {
+  // Leave force: false unless you want to summon hellfire
+  return sequelize.sync({ force: false });
+}).then(() => {
+  return sharepoint.authenticate(process.env.SHAREPOINT_USERNAME, process.env.SHAREPOINT_PASSWORD);
+}).then(() => {
+  return sharepoint.getWebEndpoint()
+}).then(async () => {
+  if (program.ormOnly) {
+    return Promise.resolve();
+  }
+  let options = {
+    // Where the program will check for files to migrate
+    outputDirectory: process.env.DELIVERY_OUTPUT_DIR
+  };
+  let app = new Application(sequelize, sharepoint, program, options);
+  const code = await app.run();
+  if (program.debug) {
+    console.log(`Exited with code ${code}`);
+  }
+  return Promise.resolve(code);
+}).catch(err => {
+  console.error(err);
+});
